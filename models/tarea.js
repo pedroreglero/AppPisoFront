8907dad5b0e40dea71d8375e45b924d3b120abd2
Pedro Reglero <pedropablo@ciclosmontecastelo.com>
2022-06-06 17:36:08 +0200

    commit

--- models/tarea.js
+++ models/tarea.js
@@ -1,5 +1,5 @@
 class Tarea {
-    constructor(id, name, created_by, created_on, status, due_to, notify_assignees, assignees) {
+    constructor(id, name, created_by, created_on, status, due_to, notify_assignees, assignees, finished_on, finished_by, cancelled_on, cancelled_by) {
         this.id = id;
         this.name = name;
         this.created_by = created_by;
@@ -8,5 +8,9 @@ class Tarea {
         this.due_to = due_to;
         this.notify_assignees = notify_assignees;
         this.assignees = assignees;
+        this.finished_on = finished_on;
+        this.finished_by = finished_by;
+        this.cancelled_on = cancelled_on;
+        this.cancelled_by = cancelled_by;
     }
 }
--- models/usuario.js
+++ models/usuario.js
@@ -1,8 +1,9 @@
 class Usuario {
-    constructor(id, username, name, hashedPassword) {
+    constructor(id, username, name, hashedPassword, assignedTasks) {
         this.id = id;
         this.name = name;
         this.username = username;
-        this.hashedPassword = hashedPassword;   
+        this.hashedPassword = hashedPassword;
+        this.assignedTasks = assignedTasks;
     }
 }
